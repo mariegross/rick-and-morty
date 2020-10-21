5bd396c8162497ba4543bd1ce6b2b79ad28eaee8
mariegross <marie.gross@gmx.de>
2020-10-21 14:59:06 +0200

    Use API for character card creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import "./app.css";
-import Header from "./components/Header";
-import { createElement } from "./utils/elements";
 import { createCharacterCard } from "./components/Characters";
+import Header from "./components/Header";
+import { getCharacterById } from "./utils/api";
+import { createElement } from "./utils/elements";
 
 function App() {
   const header = Header();
 
-  const rickSanchez = createCharacterCard({
-    name: "Rick Sanchez",
-    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  });
+  // const rickSanchez = createCharacterCard({
+  //   name: "Rick Sanchez",
+  //   imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  // });
 
-  const mortySmith = createCharacterCard({
-    name: "Morty Smith",
-    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-  });
+  // const mortySmith = createCharacterCard({
+  //   name: "Morty Smith",
+  //   imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  // });
 
-  const main = createElement("main", {
-    children: [rickSanchez, mortySmith],
-  });
+  const main = loadCharacters("main");
+
+  async function loadCharacters() {
+    const firstCharacter = await getCharacterById(1);
+    const secondCharacter = await getCharacterById(2);
+    main.append(
+      createCharacterCard({
+        name: firstCharacter.name,
+        imgSrc: firstCharacter.image,
+      }),
+      createCharacterCard({
+        name: secondCharacter.name,
+        imgSrc: secondCharacter.image,
+      })
+    );
+  }
+
+  loadCharacters();
 
   const container = createElement("div", { children: [header, main] });
   return container;
